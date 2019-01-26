202103f60afc43f870961e5295f599ab14b1fc8c
Cassio Hubner <cassiohubner@Cassios-MacBook-Pro.local>
2019-01-25 23:17:47 -0200

    :sparkles: Added views basic structure and ScrollTop component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Theme, GlobalStyle } from './theme';
 
 import Home from './views/Home';
+import Details from './views/Details';
+import NotFound from './views/NotFound';
+
+import ScrollTop from './components/ScrollTop';
 
 const App = () => (
   <ThemeProvider theme={Theme}>
     <>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact={true} component={Home} />
+          <ScrollTop>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/case/:id" component={Details} />
+            <Route path="*" component={NotFound} />
+          </ScrollTop>
         </Switch>
       </ BrowserRouter>
       <GlobalStyle />
