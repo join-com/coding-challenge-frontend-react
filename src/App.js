9b3d03c957571c69a71099cfb6bcaa909c72b982
Cassio Hubner <cassiohubner@Cassios-MacBook-Pro.local>
2019-01-25 23:06:38 -0200

    :sparkles: Basic router and theme

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import { Theme, GlobalStyle } from './theme';
+
+import Home from './views/Home';
+
+const App = () => (
+  <ThemeProvider theme={Theme}>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+        </Switch>
+      </ BrowserRouter>
+      <GlobalStyle />
+    </>
+  </ThemeProvider>
+);
 
 export default App;
