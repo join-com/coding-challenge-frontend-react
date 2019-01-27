7adbee0bf26e87ce02f438a3b8751fb461689502
Andrei Bulagin <abulagin@gmail.com>
2019-01-27 22:42:51 +0300

    add responsible layout

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -6,9 +6,38 @@ import { App } from './app';
 it('renders without crashing', () => {
   const wrapper = shallow(<App />);
   expect(wrapper).toMatchInlineSnapshot(`
-<Fragment>
-  <Header />
-  <Cases />
-</Fragment>
+<Row
+  gutter={0}
+>
+  <Col
+    lg={
+      Object {
+        "offset": 4,
+        "span": 16,
+      }
+    }
+    md={
+      Object {
+        "offset": 2,
+        "span": 20,
+      }
+    }
+    xl={
+      Object {
+        "offset": 6,
+        "span": 12,
+      }
+    }
+    xs={
+      Object {
+        "offset": 1,
+        "span": 22,
+      }
+    }
+  >
+    <Header />
+    <Cases />
+  </Col>
+</Row>
 `);
 });
--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { Col, Row } from 'antd';
 import { Cases } from './sections/cases';
 import { Header } from './sections/common/header';
 
 export const App: React.FC<{}> = () => (
-  <>
-    <Header />
-    <Cases />
-  </>
+  <Row>
+    <Col
+      xs={{ offset: 1, span: 22 }}
+      md={{ offset: 2, span: 20 }}
+      lg={{ offset: 4, span: 16 }}
+      xl={{ offset: 6, span: 12 }}
+    >
+      <Header />
+      <Cases />
+    </Col>
+  </Row>
 );
