31faba909dfa19edd65f5701c8f305e3afb7f8be
sdfg <werfsfdsf@iosdufoi.com>
2019-01-29 21:22:03 +0530

    Next clean up

--- constants.js
+++
@@ -1,8 +0,0 @@
-const APP_CONFIG = {
-    APP_CITY: 'Berlin',
-    APP_TITLE: 'Police Department of',
-    RECORDS_PER_PAGE: 10,
-    API_URL: 'https://bikewise.org/api/v2/'
-};
-
-export default APP_CONFIG;
