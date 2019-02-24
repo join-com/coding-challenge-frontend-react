ed36f6afd278a1284e50ad3c3e63a4568132a9b4
Arnaud Orieux <>
2019-02-24 21:00:54 +0100

    add history push method

--- src/global/utils/UrlHelpers.js
+++ src/global/utils/UrlHelpers.js
@@ -1,11 +1,26 @@
+import history from '../../config/route/history';
+
 export function serializeQueryParams(params) {
   return params
     && Object.keys(params).filter(key => params[key] !== '' && params[key] !== undefined)
       .map(key => `${key}=${encodeURIComponent(params[key])}`).join('&');
 }
 
+export function historyPush(pathname = '', params = {}, hash = '') {
+  if (history) {
+    const { location } = history;
+    const newPathname = pathname || (location && location.pathname);
+    const newHash = hash || (location && location.hash);
+    const newQueryString = serializeQueryParams(params);
+    const newHistoryUrl = `${newPathname}?${newQueryString}${newHash}`;
+    const newHistoryState = { params, hash };
+    history.push(newHistoryUrl, newHistoryState);
+  }
+}
+
 const UrlHelpers = {
   serializeQueryParams,
+  historyPush,
 };
 
 export default UrlHelpers;
