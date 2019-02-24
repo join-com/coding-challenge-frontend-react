decbe18e2dc1ea5d5e7166631f0e493a57fe57d5
Arnaud Orieux <>
2019-02-24 12:52:23 +0100

    add MasterWrapper to fetch content

--- src/components/master/MasterWrapper.jsx
+++ src/components/master/MasterWrapper.jsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MasterWrapper = () => <div>MasterWrapper</div>;
+import Master from './Master';
+
+const dataPagination = {
+  page: 1,
+  per_page: 10,
+};
+
+const dataFilters = {
+  incident_type: 'theft',
+  proximity: 'Berlin',
+  proximity_square: 50,
+};
+
+class MasterWrapper extends Component {
+  componentDidMount() {
+    const {
+      fetchData,
+    } = this.props;
+
+    const defaultParams = {
+      ...dataPagination,
+      ...dataFilters,
+    };
+
+    fetchData(defaultParams);
+  }
+
+  render() {
+    return (
+      <Master
+        {...this.props}
+      />
+    );
+  }
+}
 
 export default MasterWrapper;
