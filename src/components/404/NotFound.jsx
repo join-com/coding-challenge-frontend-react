9937b1ab04f74881662a10b42cb14d5d559948b4
Arnaud Orieux <>
2019-02-24 11:59:07 +0100

    add 3 main pages

---
+++ src/components/404/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import I18n from '../../utils/I18n';
+
+const NotFound = () => <div>{I18n.t('en.route.notFound')}</div>;
+
+export default NotFound;
---
+++ src/components/master/MasterPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MasterPage = () => (
+  <div>MasterPage</div>
+);
+
+export default MasterPage;
