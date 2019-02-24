4f5b53651bfdbb4282938d5cca4a339203ef697e
Arnaud Orieux <>
2019-02-24 13:39:27 +0100

    add MasterList component + test snapshots

--- src/components/master/MasterList.jsx
+++ src/components/master/MasterList.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
-const MasterList = () => <div>MasterList</div>;
+import I18n from '../../utils/I18n';
+
+import MasterListItem from './MasterListItem';
+
+const MasterList = ({ data, dataLoading }) => {
+  if (data && !dataLoading) {
+    return (
+      <div className="MasterList">
+        <div className="MasterList__container">
+          {data.length
+            ? data.map((item) => (
+              item && item.id
+                ? <MasterListItem key={item.id} item={item} />
+                : null
+              ))
+            : (
+              <div className="MasterList__list MasterList__list--empty">
+                <h4 className="MasterList__states">
+                  {I18n.t('en.data.states.empty')}
+                </h4>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 export default MasterList;
---
+++ src/components/master/MasterListItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const MasterListItem = ({ item }) => {
+  if (item) {
+    return (
+      <div className="MasterListItem">
+        <div className="MasterListItem__container">
+          {Object.keys(item).map(key => (
+            typeof item[key] !== 'object'
+              && (
+                <div className="MasterListItem__property">
+                  <div className="MasterListItem__property__name">{key}</div>
+                  <div className="MasterListItem__property__value">{item[key]}</div>
+                </div>
+              )
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default MasterListItem;
