1d98e888fd5890ae1b6d1a24f6fe334ee0bb55de
Arnaud Orieux <>
2019-02-26 21:17:35 +0100

    add DetailContainer

---
+++ src/components/detail/DetailContainer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect, Provider } from 'react-redux';
+
+import store from '../../store';
+
+import {
+  fetchDataById,
+  resetDataError,
+} from '../../actions/data';
+
+import {
+  getData,
+  getDataError,
+  getDataLoading,
+} from '../../reducers/data';
+
+const mapStateToProps = state => ({
+  data: getData(state),
+  dataError: getDataError(state),
+  dataLoading: getDataLoading(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchDataById: params => dispatch(fetchDataById(params)),
+  resetDataError: () => dispatch(resetDataError()),
+});
+
+const ConnectedDetailContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DetailWrapper);
+
+const DetailContainer = props => (
+  <Provider store={store}>
+    <ConnectedDetailContainer {...props} />
+  </Provider>
+);
+
+export default DetailContainer;
