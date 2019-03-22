53698f7879d39197fa33da24accb5fa15b8290d9
aiyk <aiyk.ekwe@gmail.com>
2019-03-22 13:52:50 +0100

    list design updates

--- stolen-bike-client-app/src/components/incident-list/incident-list-item/IncidentListItem.js
+++ stolen-bike-client-app/src/components/incident-list/incident-list-item/IncidentListItem.js
@@ -14,11 +14,9 @@ const IncidentListItem = ({ item }) => {
                 <div className="listItem__content__title">2017 CANYON BICYCLES INFLITE AL 9.0S</div>
                 <div className="listItem__content__others">Bike was locked to a bike rack with an "Asus Bordo Granit X-Plus" lock. After two hours in the gym, it was gone, no trace left. The rack was a bit loose in the ground, it's possible that the thieves tried to pull it out, but failed due to the concrete foundation.</div>
                 <div className="listItem__content__footer">
-                    <div>
-                        <div className="listItem__content__others">Stolen <strong>Mar 9th, 3pm WAT at Berlin, 10117, DE</strong></div>
-                        <div className="listItem__content__others">Reported <strong>Mar 9th, 3pm WAT</strong></div>
-                    </div>
-                    <div className="btn btn-primary">Details</div>
+                    <div className="listItem__content__others">Stolen <strong>Mar 9th, 3pm WAT at Berlin, 10117, DE</strong></div>
+                    <div className="listItem__content__others">Reported <strong>Mar 9th, 3pm WAT</strong></div>
+                    {/* <div className="btn btn-primary">Details</div> */}
                 </div>
             </div>
         </div>
