cd2e941e9a145bcd2984ceb0c0f05d387a2e3cf4
eldar <eldar.lazhuyeu@bpol.net>
2019-04-15 20:44:53 +0200

    Minor header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-import Logo from './logo.png'
+import LogoImage from './logo.png'
+
+import {
+  Container,
+  Logo
+} from './styled'
 
 const Header = () => (
-  <div>
-    <img src={Logo} alt="logo" />
-  </div>
+  <Container>
+    <Logo src={LogoImage} alt="logo" />
+    <div>
+      <h1>Police Department of Berlin</h1>
+      <h3>Stolen Bikes</h3>
+    </div>
+  </Container>
 )
 
 export default Header
---
+++ src/components/Header/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Logo = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 0 20px;
+`
+
+export { Container, Logo }
