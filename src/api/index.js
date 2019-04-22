98d18d03cccd5ee79ea9403c960edaa1208ac673
eldar <eldar.lazhuyeu@bpol.net>
2019-04-22 22:26:10 +0200

    Fetch incident by ID and fetch coordinates

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,13 @@
 export const getIncidents = () => (
   fetch(`https://bikewise.org/api/v2/incidents?incident_type=theft&page=1&per_page=1000&proximity=USA`)
 )
+
+export const fetchIncident = id => (
+  fetch(`https://bikewise.org/api/v2/incidents/${id}`)
+)
+
+export const fetchLocations = (timestamp, title) => (
+  fetch(
+    `https://bikewise.org/api/v2/locations?incident_type=theft&occurred_before=${timestamp}&occurred_after=${timestamp}&query=${title}`
+  )
+)
--- src/views/Details/Details.js
+++ src/views/Details/Details.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react'
+import { fetchIncident, fetchLocations } from '../../api'
 
 class Details extends Component {
+  state = {
+    incident: {},
+    coordinates: [0, 0]
+  }
+
+  componentDidMount () {
+    //Fetching incident by id
+    fetchIncident(this.props.match.params.id)
+      .then(response => response.json())
+      .then(({ incident }) => {
+        this.setState({incident})
+        
+        //Now when we have all required data, we can fetch coordinates
+        fetchLocations(incident.occurred_at, incident.title)
+          .then(response => response.json())
+          .then(json => json.features[0].geometry.coordinates)
+          .then(coordinates => {
+            this.setState({coordinates})
+          })
+          .catch(error => console.log(error))
+      })
+      .catch(error => console.log(error))
+  }
+
   render () {
     return (
       <div>
