7b0433e2fa7b23ba38f2fc276537e2321d36acd2
Ankit Sharma <ankit.sharma.15@alight.com>
2019-05-07 14:05:10 +0530

    initial commit
    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Berlin Police Departement</h1>
+            <h2>Stolen Byke Index</h2>
+        </div>
+    );
+}
+
+export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
