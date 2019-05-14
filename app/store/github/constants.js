153df20008dba8192a470b544c24223cd4e7b16e
Alex Elgin <sascha.elgin@gmail.com>
2019-05-14 17:26:06 +0600

    delete unused constant

--- app/store/github/constants.js
+++ app/store/github/constants.js
@@ -1,6 +1,5 @@
 export const REPOSITORIES_FORM = 'forms/github/REPOSITORIES_FORM';
 
-export const CHANGE_USERNAME = 'actions/github/CHANGE_USERNAME';
 export const LOAD_REPOS = 'actions/github/LOAD_REPOS';
 export const LOAD_REPOS_ERROR = 'actions/github/LOAD_REPOS_ERROR';
 export const LOAD_REPOS_SUCCESS = 'actions/github/LOAD_REPOS_SUCCESS';
