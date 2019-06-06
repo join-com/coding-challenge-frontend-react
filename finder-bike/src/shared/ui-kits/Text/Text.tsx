d6f1899dffe29e909312cc36b1cd47bb97e8db91
tai <tai@tais-MacBook-Pro.local>
2019-06-06 15:30:44 +0700

    fix(Text): Fix font for Text component

--- finder-bike/src/shared/ui-kits/Text/Text.tsx
+++ finder-bike/src/shared/ui-kits/Text/Text.tsx
@@ -17,6 +17,10 @@ const TextWithoutLink = styled.span<IText>`
   font-weight: ${props => (props.isBold ? 'bold' : 'normal')};
   font-size: ${({ size }) => (size ? `${size}px` : '1.3rem')};
   display: ${({ isBlock }) => (isBlock ? 'block' : 'inline-block')};
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+    'Segoe UI Symbol';
+  line-height: 1.5;
 `;
 
 const TextWithLink = styled.a`
@@ -25,6 +29,10 @@ const TextWithLink = styled.a`
   font-weight: ${props => (props.isBold ? 'bold' : 'normal')};
   font-size: ${({ size }) => (size ? `${size}px` : '1.3rem')};
   display: ${({ isBlock }) => (isBlock ? 'block' : 'inline-block')};
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+    'Segoe UI Symbol';
+  line-height: 1.5;
 `;
 
 function Text(props: IText) {
