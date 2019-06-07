02f960b00ac3820074cedd72784e506e42724dd7
iamawebgeek <iamawebgeek@gmail.com>
2019-06-07 11:18:33 +0500

    feat: Integrate prettier

---
+++ app/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'typescript',
+}
