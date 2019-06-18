f29454588d2b106935959ce2190ab1ad5c756010
alex <alexandre.xypas@gmail.com>
2019-06-18 09:16:58 +0300

    Style search button

--- src/pages/List/SearchButton/SearchButton.js
+++ src/pages/List/SearchButton/SearchButton.js
@@ -1,11 +1,11 @@
-
 import React from 'react'
 import styled from 'styled-components'
 
-
 const Button = styled.button`
   background-color: 'blue';
+  margin-left: 15px;
+  margin-right: 15px;
 `
-export default function SearchButton({onClick}) {
+export default function SearchButton({ onClick }) {
   return <Button onClick={onClick}>Find cases</Button>
 }
