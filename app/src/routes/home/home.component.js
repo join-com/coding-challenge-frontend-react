c1f38c2260830b2c462dc415725c22b855764106
Khaldi Ameur <khaldi.ameur1995@gmail.com>
2019-06-30 23:43:10 +0100

    Add Index page. ( Home )

--- app/src/routes/home/home.component.js
+++ app/src/routes/home/home.component.js
@@ -1,32 +1,128 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-
-import { FormattedMessage } from 'react-intl';
+import { Skeleton, Form } from 'antd';
+import defaultBike from '../../images/icons/logo.svg';
 
 import messages from './home.messages';
-import { LanguageSwitcher } from '../../shared/components/languageSwitcher';
-import { Container, Logo } from './home.styles';
-import { H1 } from '../../theme/typography';
+import {
+  Container,
+  ContainerLeftAlign,
+  ContainerHeader,
+  StyledList,
+  StyledDatePicker as RangePicker,
+  StyledButton as Button,
+  StyledInput as Input,
+  StyledImage,
+} from './home.styles';
+import { H1, H2 } from '../../theme/typography';
 
 export class Home extends PureComponent {
   static propTypes = {
     intl: PropTypes.object.isRequired,
     language: PropTypes.string.isRequired,
+    loadList: PropTypes.func.isRequired,
+    selectBikeInfo: PropTypes.func.isRequired,
+    list: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.any,
+    history: PropTypes.any,
+    getFieldDecorator: PropTypes.func,
+    form: PropTypes.object,
   };
-
+  componentDidMount() {
+    this.props.loadList(1, 1000, 'theft', 'berlin', null, null, null);
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const { date, search } = values;
+        if (date && date.length) {
+          const [startDate, endDate] = date;
+          this.props.loadList(1, 1000, 'theft', 'berlin', search, startDate.unix(), endDate.unix());
+        } else {
+          this.props.loadList(1, 1000, 'theft', 'berlin', search, null, null);
+        }
+      }
+    });
+  };
   render() {
+    const { list, error, loading } = this.props;
+    const { getFieldDecorator } = this.props.form;
     return (
       <Container>
         <Helmet title={this.props.intl.formatMessage(messages.pageTitle)} />
-
-        <H1>
-          <FormattedMessage {...messages.welcome} />
-        </H1>
-
-        <Logo />
-
-        <LanguageSwitcher />
+        <ContainerHeader>
+          <H1> Police Department of Berlin</H1>
+          <H2> Stolen bikes</H2>
+        </ContainerHeader>
+        <ContainerHeader>
+          <Form layout="inline" onSubmit={this.handleSubmit}>
+            <Form.Item>
+              {getFieldDecorator('search', {
+                rules: [{ required: false }],
+              })(<Input placeholder="Word to look for in cases..." />)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('date', {
+                rules: [{ required: false }],
+              })(<RangePicker />)}
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" disabled={loading}>
+                Search cases
+              </Button>
+            </Form.Item>
+          </Form>
+        </ContainerHeader>
+        {!error && (
+          <ContainerLeftAlign>
+            <StyledList
+              itemLayout="vertical"
+              size="large"
+              pagination={{
+                pageSize: 10,
+                total: this.props.list.length,
+              }}
+              dataSource={list}
+              loading={loading}
+              footer={
+                <div id="total-cases">
+                  <b>Total of {this.props.list.length}</b>
+                </div>
+              }
+              header={
+                <div>
+                  <b>Total of {this.props.list.length}</b>
+                </div>
+              }
+              renderItem={item => (
+                <StyledList.Item
+                  onClick={() => {
+                    this.props.selectBikeInfo(item);
+                    this.props.history.push(`/${this.props.language}/incident/${item.id}`);
+                  }}
+                  key={item.id}
+                  extra={!loading && <StyledImage alt="logo" src={item.media.image_url || defaultBike} />}
+                >
+                  <Skeleton loading={loading} active avatar>
+                    <StyledList.Item.Meta
+                      title={<a href={item.href}>{item.title}</a>}
+                      description={`${item.address} on ${new Date(item.occurred_at * 1000).toLocaleDateString()} `}
+                    />
+                    <p> {item.description || 'No description... '}</p>
+                    <p>
+                      Updated at:
+                      <strong> {new Date(item.updated_at * 1000).toLocaleString()} </strong>
+                    </p>
+                  </Skeleton>
+                </StyledList.Item>
+              )}
+            />
+          </ContainerLeftAlign>
+        )}
+        {error && error.error && <H2> Error happened, Please try again.</H2>}
       </Container>
     );
   }
--- app/src/routes/home/home.styles.js
+++ app/src/routes/home/home.styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ReactComponent as LogoSVG } from '../../images/icons/logo.svg';
+import { DatePicker, Input, Button, List } from 'antd';
 
 export const Container = styled.div`
   display: flex;
@@ -7,9 +7,43 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
+  background-color: #ffffff;
 `;
 
-export const Logo = styled(LogoSVG)`
-  width: 100px;
-  margin-bottom: 20px;
+export const ContainerLeftAlign = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 200px;
+  text-align: left;
+`;
+
+export const ContainerHeader = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  width: 100%;
+  background-color: #fafafa;
+`;
+
+export const StyledList = styled(List)`
+  width: calc(100vw - 400px);
+`;
+
+export const StyledInput = styled(Input)`
+  margin: 10px;
+`;
+
+export const StyledDatePicker = styled(DatePicker.RangePicker)`
+  margin: 10px;
+`;
+export const StyledButton = styled(Button)`
+  margin: 10px;
+`;
+export const StyledImage = styled.img`
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
 `;
