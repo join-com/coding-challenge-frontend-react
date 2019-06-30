f1e52f53e90b4c46ea2ae4b57c899e1b737c562e
Khaldi Ameur <khaldi.ameur1995@gmail.com>
2019-06-30 23:32:07 +0100

    Link Home container with incidents module.

--- app/src/routes/home/home.container.js
+++ app/src/routes/home/home.container.js
@@ -5,15 +5,31 @@ import { hot } from 'react-hot-loader';
 import { injectIntl } from 'react-intl';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'ramda';
+import { Form } from 'antd';
 
 import { Home } from './home.component';
 import { selectLocalesLanguage } from '../../modules/locales';
+import { IncidentsActions } from '../../modules/incidents';
+import {
+  selectListDomain,
+  selectLoadingDomain,
+  selectIncidentsErrorDomain,
+} from '../../modules/incidents/incidents.selectors';
 
 const mapStateToProps = createStructuredSelector({
   language: selectLocalesLanguage,
+  list: selectListDomain,
+  loading: selectLoadingDomain,
+  error: selectIncidentsErrorDomain,
 });
-
-export const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+export const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      loadList: IncidentsActions.requestBikeThefts,
+      selectBikeInfo: IncidentsActions.showBikeDetails,
+    },
+    dispatch
+  );
 
 export default compose(
   hot(module),
@@ -22,5 +38,6 @@ export default compose(
     mapDispatchToProps
   ),
   injectIntl,
-  withRouter
+  withRouter,
+  Form.create({ name: 'cases-search' })
 )(Home);
