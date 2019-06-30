807b6589514e4774d9a5d68cdbe5edfccb113c79
Khaldi Ameur <khaldi.ameur1995@gmail.com>
2019-07-01 00:24:45 +0100

    Link bikeInfo component with incidents module.

--- app/src/routes/bikeInfo/bikeInfo.container.js
+++ app/src/routes/bikeInfo/bikeInfo.container.js
@@ -4,12 +4,21 @@ import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 import { compose } from 'ramda';
-
+import {
+  selectSelectedBikeDomain,
+  selectLoadingDomain,
+  selectIncidentsErrorDomain,
+} from '../../modules/incidents/incidents.selectors';
+import { IncidentsActions } from '../../modules/incidents';
 import { BikeInfo } from './bikeInfo.component';
 
-const mapStateToProps = createStructuredSelector({});
+const mapStateToProps = createStructuredSelector({
+  bike: selectSelectedBikeDomain,
+  loading: selectLoadingDomain,
+  error: selectIncidentsErrorDomain,
+});
 
-export const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+export const mapDispatchToProps = dispatch => bindActionCreators(IncidentsActions, dispatch);
 
 export default compose(
   hot(module),
