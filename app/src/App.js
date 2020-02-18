35ae67dd4f316298de9b45f51fc54f188476a866
Adam Zerner <adamzerner@protonmail.com>
2020-02-17 19:35:25 -0800

    Keep SSoT in sync with Filters component

--- app/src/App.js
+++ app/src/App.js
@@ -17,21 +17,37 @@ export default class App extends React.Component {
       fromDate: null,
       toDate: null,
     };
+    this.updateSearchText = this.updateSearchText.bind(this);
+    this.updateFromDate = this.updateFromDate.bind(this);
+    this.updateToDate = this.updateToDate.bind(this);
     this.setCases = this.setCases.bind(this);
   };
-
+  updateSearchText(e) {
+    this.updateState({
+      searchText: e.target.value,
+    });
+  };
+  updateFromDate(e) {
+    this.updateState({
+      fromDate: e.target.value,
+    });
+  };
+  updateToDate(e) {
+    this.updateState({
+      toDate: e.target.value,
+    });
+  };
   updateState(o) {
     let newState = {};
 
     if (o.loading === true || o.loading === false) { newState.loading = o.loading; }
     if (o.pageNum) { newState.pageNum = o.pageNum; }
-    if (o.searchText) { newState.searchText = o.searchText; }
+    if (o.searchText || o.searchText === '') { newState.searchText = o.searchText; }
     if (o.fromDate) { newState.fromDate = o.fromDate; }
     if (o.toDate) { newState.toDate = o.toDate; }
 
     this.setState(newState);
   };
-
   getUrlForSettingCases() {
     let url = `https://bikewise.org:443/api/v2/incidents?page=${this.state.pageNum}&per_page=10&incident_type=theft&proximity=Berlin%2C%20DE&proximity_square=100`;
 
@@ -41,7 +57,6 @@ export default class App extends React.Component {
 
     return url;
   };
-
   setCases(o) {
     if (this.state.loading) {
       return; // TODO: disable links when loading
@@ -62,18 +77,31 @@ export default class App extends React.Component {
       })
     ;
   };
-
   componentDidMount() {
     this.setCases({ pageNum: 1 });
   };
-
   render() {
     return (
       <section className="container">
         <Header />
-        <Filters applyFilters={this.setCases}/>
-        <Cases cases={this.state.cases} loading={this.state.loading}/>
-        <Pagination currPage={this.state.pageNum} numPages={5} setCases={this.setCases} />
+        <Filters
+          searchText={this.state.searchText}
+          fromDate={this.state.fromDate}
+          toDate={this.state.toDate}
+          updateSearchText={this.updateSearchText}
+          updateFromDate={this.updateFromDate}
+          updateToDate={this.updateToDate}
+          setCases={this.setCases}
+        />
+        <Cases
+          cases={this.state.cases}
+          loading={this.state.loading}
+        />
+        <Pagination
+          currPage={this.state.pageNum}
+          numPages={5}
+          setCases={this.setCases}
+        />
       </section>
     )
   };
--- app/src/components/filters/Filters.js
+++ app/src/components/filters/Filters.js
@@ -5,16 +5,37 @@ export default class Filters extends React.Component {
   render() {
     return (
       <section className="flex-container">
-        <input type="text" className="form-control" placeholder="Search case descriptions" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search case descriptions"
+          value={this.props.searchText}
+          onChange={this.props.updateSearchText}
+        />
         <span>
           From:
-          <input type="date" className="form-control" />
+          <input
+            type="date"
+            className="form-control"
+            value={this.props.fromDate}
+            onChange={this.props.updateFromDate}
+          />
         </span>
         <span>
           To:
-          <input type="date" className="form-control" />
+          <input
+            type="date"
+            className="form-control"
+            value={this.props.toDate}
+            onChange={this.props.updateToDate}
+          />
         </span>
-        <button type="button" className="btn btn-primary">Find cases</button>
+        <button
+          type="button"
+          className="btn btn-primary"
+        >
+          Find cases
+        </button>
       </section>
     )
   }
